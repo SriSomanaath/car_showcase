f0964870029c604cb0cf51dbec748be22d11d1ac
SriSomanaath <govindalapudisrinath@gmail.com>
2023-12-08 12:22:12 +0530

    solved error

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,12 +3,12 @@
 import React from 'react'
 import Image from "next/image";
 import { Fragment, useState } from "react";
-import {  SearchManufacturerProps } from '@/types'
+import {  SearchManuFacturerProps } from '@/types'
 import { Combobox, Transition } from "@headlessui/react";
 
 import { manufacturers } from "@/constants";
 
-const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
+const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
 
   const filteredManufacturers =
